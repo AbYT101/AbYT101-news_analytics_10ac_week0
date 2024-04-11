fd3095129a76660bad09e0f378df865797de8178
abyt101 <abrerhamyirsaw@gmail.com>
2024-04-11 19:02:28 +0300

    test cases added

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,23 @@
 import unittest
+from src.loader import NewsDataLoader
+import pandas as pd
 
 class Test(unittest.TestCase):
-    def test_example(self):
-        # Example test case
-        self.assertEqual(1 + 1, 2)
+    
+    def setUp(self):
+        self.loader = NewsDataLoader()
 
-    # Add more test cases as needed
-    # def test_another_example(self):
-    #     self.assertEqual(2 * 3, 6)
+    def test_get_news(self):
+        expected_file = pd.read_csv('../data/rating.csv')
+        self.assertEqual(self.loader.get_news_data(), expected_file)
+
+    def test_get_traffic(self):
+        expected_file = pd.read_csv('../data/traffic.csv')
+        self.assertEqual(self.loader.get_traffic_data(), expected_file)
+
+    def test_get_domain_location(self):
+        expected_file = pd.read_csv('../data/traffic.csv')
+        self.assertEqual(self.loader.get_domain_location_data(), expected_file)
 
 if __name__ == '__main__':
     unittest.main()
